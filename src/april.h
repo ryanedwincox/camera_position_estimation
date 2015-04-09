978a70a640e278dd7e6b008a7339212c01622e16
Ryan Cox <ryanedwincox@gmail.com>
2015-04-08 17:37:52 -0700

    cleaned up code a bit

--- src/april.h
+++ src/april.h
@@ -18,14 +18,21 @@ public:
     void setup(ros::NodeHandle nh);
     void loop();
     void processImage(cv::Mat& image, cv::Mat& image_gray);
-    void publishCameraTF(cv::Mat rMat, cv::Mat tvec);
+    void publishCameraTF(cv::Mat rMat, cv::Mat tVec);
     void publishMarkerTF();
-    cv::Mat projectAxis(cv::Mat img, Eigen::Matrix3d rot, Eigen::Vector3d trans);
+    cv::Mat projectAxis(cv::Mat img);
 
 private:
     AprilTags::TagDetector* tagDetector;
     cv::VideoCapture* cap;
     ros::NodeHandle nh;
+    cv::Mat cameraMatrix;
+    cv::Mat distCoeffs;
+    cv::Mat rvec;
+    cv::Mat tvec;
+//    std::vector<cv::Point3f> objPts;
+//    std::vector<cv::Point2f> imgPts;
+    double tag_size;
 };
 
 #endif // APRIL_H
